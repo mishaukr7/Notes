a24ce14561b76c186f2602002bc0daab4285ccd2
Mykhailo Khomenko <mishaukr22@gmail.com>
2018-08-03 13:06:14 +0300

    Update settings.py
    Edit database settings

--- testproject/settings.py
+++ testproject/settings.py
@@ -63,17 +63,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'testproject.wsgi.application'
 
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': config('DB_HOST'),
-#         'PORT': '',
-#     }
-# }
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
